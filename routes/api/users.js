0892ca72d96ca9cfdaf56afb179460a82a076f1f
Odysseas Kouloukythas <19oddy91@gmail.com>
2021-04-17 14:52:39 +0300

    Implemented JWT.

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -22,6 +24,7 @@ router.post('/',
         }
 
         const { name, email, password } = req.body;
+        
 
         try {
           
@@ -54,9 +57,22 @@ router.post('/',
 
             await user.save();
 
-            // Return jsonwebtoken 
-            
-            res.send('User registered');
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+            { expiresIn: 360000 },
+            (err, token) => {
+                if(err) throw err;
+                res.json({ token });
+            }
+            );
+
 
         } catch (err) {
             console.error(err.message);
