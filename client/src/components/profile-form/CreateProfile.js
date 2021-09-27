9e2f14535120d843f78c671bf8102deec51172cd
Odysseas Kouloukythas <19oddy91@gmail.com>
2021-09-27 21:09:52 +0300

    Added Create Profile Action

--- client/src/components/profile-form/CreateProfile.js
+++ client/src/components/profile-form/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useState } from 'react';
+import { Link, withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { createProfile } from '../../actions/profile';
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
         const [formData, setFormData] = useState({
             company: '',
             website: '',
@@ -37,6 +39,11 @@ const CreateProfile = props => {
 
         const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
 
+        const onSubmit = e => {
+            e.preventDefault();
+            createProfile(formData, history)
+        }
+
         return (
             <Fragment>
                 <h1 className="large text-primary">
@@ -47,7 +54,7 @@ const CreateProfile = props => {
                     profile stand out
                 </p>
                 <small>* = required field</small>
-                <form className="form">
+                <form className="form" onSubmit={e => onSubmit(e)}>
                     <div className="form-group">
                     <select name="status" value={status} onChange={e => onChange(e)}>
                         <option value="0">* Select Professional Status</option>
@@ -71,7 +78,7 @@ const CreateProfile = props => {
                     >
                     </div>
                     <div className="form-group">
-                    <input type="text" placeholder="Website" name="website" value={company} onChange={e => onChange(e)} />
+                    <input type="text" placeholder="Website" name="website" value={website} onChange={e => onChange(e)} />
                     <small className="form-text"
                         >Could be your own or a company website</small
                     >
@@ -148,8 +155,8 @@ const CreateProfile = props => {
 } 
 
 CreateProfile.propTypes = {
-
+    createProfile: PropTypes.func.isRequired,
 };
 
-export default CreateProfile;
+export default connect(null, { createProfile })(withRouter(CreateProfile));
 
